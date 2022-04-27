0d7c8075b47fb8dd1df48953dd73be67ecc65c29
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-27 16:37:19 +0200

    first commit

---
+++ script.rb
@@ -0,0 +1,6 @@
+# Process : Write a script where asks for a confirmation when deleting a folder if this folder contains a file .important
+# If the user confirms the deletion, the folder is deleted.
+# If the user cancels the deletion, the folder is not deleted.
+# Author: Guillaume MORET
+# --------------------------------- 
+
