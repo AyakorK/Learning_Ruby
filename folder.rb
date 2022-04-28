9429acc1cb83d1d8570d863bb45f05fc96453353
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-28 16:46:04 +0200

    Correcting bug o f returning an array if no return statement was specified + placing in a variabl e the target file

--- folder.rb
+++ folder.rb
@@ -1,66 +1,65 @@
 require 'fileutils'
 
 class Folder
+  $file_to_del = ".important"
 
   def self.check_folder(folder)
 
     return false if !File.directory?(folder)
 
     folders = Dir.entries(folder)
-
     return true if check_important(folders)
-      return true
-    end
 
     folders.each do |f|
       next if f.include?(".")
       if File.directory?("#{folder}/#{f}")
         subfolder= "#{folder}/#{f}"
         puts "Found subfolder: #{subfolder}"
-        puts "Found subfolder: " + subfolder
+        puts check_folder(subfolder)
         return true if check_folder(subfolder)
       end
     end
+    return false
   end
 
   def self.check_important(folders)
     #puts "Checking important files in folder: " + folders.to_s
-    return false unless folders.include?(".important")
-    
-    puts "Found '.important' file"
-    true
-      puts "Found .important file"
+    return false unless folders.include?("#{$file_to_del}")
+      puts "Found #{$file_to_del} file"
       return true
     end
-  end
 
   def self.ask_answer(question)
     puts question
     return gets.chomp
   end
 
-
   def self.delete_folder(folder = null)
+    responses = ["y", "n", "Y", "N"]
 
     if !File.directory?(folder)
       puts "Folder does not exist"
       return
     end
 
-    if check_folder(folder).inspect == "true"
-      question = "This folder #{folder} contains a .important file, do you want to delete it? (y/n)"
+    if check_folder(folder)
+      question = "This folder #{folder} contains a #{$file_to_del} file, do you want to delete it? (y/n)"
       answer = ask_answer(question)
-      while answer != 'y' && answer != 'n' && answer != 'Y' && answer != 'N' do
+
+      while !responses.include?(answer) do
+        hasAnswered = true
         answer = ask_answer("Wrong answer pick between y/n \n#{question}")
-        if answer == 'y' || answer == 'Y'
-          puts "Deleting folder #{folder}"
-          FileUtils.rm_rf(folder)
-        end
-        puts "Aborting" if answer == 'n' || answer == 'N'
       end
+      
+      if answer == 'y' || answer == 'Y'
+        puts "Deleting folder #{folder}"
+        #FileUtils.rm_rf(folder)
+      end
+      puts "Aborting" if answer == 'n' || answer == 'N'
+      
     else
       puts "Deleting folder #{folder}"
-      FileUtils.rm_rf(folder)
+      #FileUtils.rm_rf(folder)
     end
   end
 end
